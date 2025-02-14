fbf70e9bd0b19c266f28f38cc24f834841918c64
ITIT <itit@iMac-de-ULSA.local>
2025-02-13 19:31:17 -0600

    Primer commit

---
+++ mobile2SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobile2SwiftUI
+//
+//  Created by ITIT on 13/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile2SwiftUI/mobile2SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile2SwiftUIApp.swift
+//  mobile2SwiftUI
+//
+//  Created by ITIT on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct mobile2SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
