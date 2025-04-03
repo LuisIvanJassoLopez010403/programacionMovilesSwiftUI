4faf4b4efab2252947c294e360e63d22d912c8dc
ITIT <itit@iMac-de-ULSA.local>
2025-04-02 19:48:52 -0600

    Se termina vista de seleccion de tema al agitar

--- mobile2SwiftUI/SecondPartial/Shake/ViewModel/ShakeViewModel.swift
+++ mobile2SwiftUI/SecondPartial/Shake/ViewModel/ShakeViewModel.swift
@@ -5,32 +5,32 @@
 //  Created by ITIT on 01/04/25.
 //
 import Foundation
-
 import SwiftUI
 
 class ShakeViewModel: ObservableObject {
     @Published var materias = [
-            "Diseño de Programas",
-            "Lenguajes de Programación",
-            "Sistemas Operativos",
-            "Redes",
-            "Ingeniería de Software",
-            "Aplicaciones Móviles",
-            "Programación Gráfica",
-            "Inteligencia Artificial",
-            "Bases de datos, Big Data, Inteligencia de Negocios",
-            "Seguridad en redes y tecnologías de información",
-            "Programación para Internet"
-        ]
+        "Diseño de Programas",
+        "Lenguajes de Programación",
+        "Sistemas Operativos",
+        "Redes",
+        "Ingeniería de Software",
+        "Aplicaciones Móviles",
+        "Programación Gráfica",
+        "Inteligencia Artificial",
+        "Bases de datos, Big Data, Inteligencia de Negocios",
+        "Seguridad en redes y tecnologías de información",
+        "Programación para Internet"
+    ]
     
     func getMateria() -> String? {
-            guard !materias.isEmpty else { return nil }
-            
-            let randomIndex = Int.random(in: 0..<materias.count)
-            let materiaSeleccionada = materias[randomIndex]
-            
-            materias.remove(at: randomIndex)
-            
-            return materiaSeleccionada
-        }
+        guard !materias.isEmpty else { return nil }
+        
+        let randomIndex = Int.random(in: 0..<materias.count)
+        let materiaSeleccionada = materias[randomIndex]
+        
+        materias.remove(at: randomIndex)
+        
+        return materiaSeleccionada
+    }
 }
+
--- mobile2SwiftUI/SecondPartial/Shake/Views/ShakeView.swift
+++ mobile2SwiftUI/SecondPartial/Shake/Views/ShakeView.swift
@@ -6,20 +6,66 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ShakeView: View {
     @StateObject var viewModel = ShakeViewModel()
+    @State private var materiaSeleccionada: String? = nil
+    @State private var noMasCategorias = false
     
     var body: some View {
         VStack {
-            Text("Materia aleatoria: \(viewModel.materias.first ?? "No hay más materias")")
-                .padding()
+            Image("logoLaSalle")
+                .resizable()
+                .frame(width: 300, height: 100)
+                .padding(5)
+            
+            Text("Ingeniería en Tecnologías de la Información y Telecomunicaciones")
                 .font(.title)
+                .padding()
+                .multilineTextAlignment(.center)
+                .foregroundColor(.blue)
+            
+            LottieView(animation: .named("animation"))
+                .playing(loopMode: .loop)
+            
+            if noMasCategorias {
+                Text("Ya no hay más categorías.")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.blue)
+            } else if let materia = materiaSeleccionada {
+                Text("Categoría seleccionada: ")
+                    .font(.headline)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.blue)
+                Text("\(materia)")
+                    .font(.headline)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.blue)
+            } else {
+                Text("Agite para seleccionar una categoría.")
+                    .font(.headline)
+                    .padding()
+                    .foregroundColor(.blue)
+            }
         }
         .onShake {
             if let materia = viewModel.getMateria() {
-                print("Nueva materia seleccionada: \(materia)")
+                materiaSeleccionada = materia
+                print("Categoría eliminada: \(materia)")
+            } else {
+                noMasCategorias = true
+                print("Ya no hay más categorías.")
             }
         }
     }
 }
+
+struct ShakeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShakeView()
+    }
+}
+
